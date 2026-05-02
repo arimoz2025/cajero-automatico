83b0f0c8cb9fa9ddaa6dd933ae7abd06897e8248
i_creb <eecvasquez17@gmail.com>
2026-05-02 02:09:36 -0600

    Cambios en program.cs
    Sin llamado a base de datos pues no existía conexión y saltaba a Menu.Iniciar

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using System;
+using CajeroAutomatico.Base_de_Datos;
 using CajeroAutomatico.UI;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.Title = "BancoSim ATM — Universidad Don Bosco";
+
+ConexionDB.InicializarBaseDeDatos();
 
 Menu.Iniciar();
